7e137fff8f528b47cccba1b6c8821027e1fe2b93
Emelianenkosa <emelianenko@tut.by>
2022-10-12 23:12:21 +0300

    js event and foreach

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 
-//open/close aside nav menu
-let navTitle = document.querySelectorAll('.nav-title-item');
-let navSubtitleList = document.querySelectorAll('.nav-subtitle-list');
 
-for (let i=0; i<navTitle.length; i++) {
-    navTitle[i].addEventListener('click', () => {
-        navTitle[i].classList.toggle('nav-title-active');
-        navSubtitleList[i].classList.toggle('nav-subtitle-active');
-    });
-}   
+//open/close aside nav menu
+// let navTitle = document.querySelectorAll('.nav-title-item');
+// let navSubtitleList = document.querySelectorAll('.nav-subtitle-list');
+
+// for (let i=0; i<navTitle.length; i++) {
+//     navTitle[i].addEventListener('click', () => {
+//         navTitle[i].classList.toggle('nav-title-active');
+//         navSubtitleList[i].classList.toggle('nav-subtitle-active');
+//     });
+// }
+const navTitleList = document.querySelector('.nav-title-list');
+
+navTitleList.addEventListener('click', navTitleListOnClick);
+
+function navTitleListOnClick(event) {
+    const node = event.target;
+    const element = node.querySelector('.nav-subtitle-list');
+    if (!element) {
+        return
+    } else {
+        element.classList.toggle('nav-subtitle-active');
+        node.classList.toggle('nav-title-active');
+    };
+
+};
+    
 
 //open/close sort window
 let sortWindow = document.querySelector('.window-up-sort');
@@ -25,37 +42,100 @@ closeSort.addEventListener('click', () =>  {
     sortArrow.classList.remove('active-arrow-up');
 });
 
-// in filter window
-let filterSizeItem = document.querySelectorAll('.filter-size-item');
-for (let i=0; i<filterSizeItem.length; i++) {
-    filterSizeItem[i].addEventListener('click', () => {
-        filterSizeItem[i].classList.toggle('size-red');
-    });
-}
+
+// // open filter window
+// let filterBtnOpen = document.querySelectorAll('.catalog-filter-btn');
+// let filterBtnSet = document.querySelectorAll('.filter-btn-set');
+// let filterWindow = document.querySelectorAll('.filter-window-up');
+// let filtrArrow = document.querySelectorAll('.filter-arrow');
+// for (let i=0; i<filterBtnOpen.length; i++) {
+//     filterBtnOpen[i].addEventListener('click', () => {
+//         for (let j=0; j<filterSizeItem.length; j++) {           //сбрасывает красный цвет при открытии
+//                 filterSizeItem[j].classList.remove('size-red');
+//             };
+//         filterWindow[i].classList.toggle('filter-window-active');
+//         filtrArrow[i].classList.toggle('active-arrow-up');
+//     });
+// }    
+// // close filter window    
+// for (let i=0; i<filterBtnSet.length; i++ ) {
+//     filterBtnSet[i].addEventListener('click', () => {
+//         filterWindow[i].classList.remove('filter-window-active');
+//         filtrArrow[i].classList.toggle('active-arrow-up');
+//     });
+// };
+    
 
 // open filter window
-let filterBtnOpen = document.querySelectorAll('.catalog-filter-btn');
-//let filterBtnSet = document.getElementsByClassName('filter-btn-set');
-let filterBtnSet = document.querySelectorAll('.filter-btn-set');
-let filterWindow = document.querySelectorAll('.filter-window-up');
-let filtrArrow = document.querySelectorAll('.filter-arrow');
-for (let i=0; i<filterBtnOpen.length; i++) {
-    filterBtnOpen[i].addEventListener('click', () => {
-        for (let j=0; j<filterSizeItem.length; j++) {           //сбрасывает красный цвет при открытии
-                filterSizeItem[j].classList.remove('size-red');
-            };
-        filterWindow[i].classList.toggle('filter-window-active');
-        filtrArrow[i].classList.toggle('active-arrow-up');
-    });
-}    
-// close filter window    
-for (let i=0; i<filterBtnSet.length; i++ ) {
-    filterBtnSet[i].addEventListener('click', () => {
-        filterWindow[i].classList.remove('filter-window-active');
-        filtrArrow[i].classList.toggle('active-arrow-up');
-    });
+const filterList = document.querySelector('.catalog-filter-list');
+
+filterList.addEventListener('click', filterListOnClick);
+
+function filterListOnClick(event) {
+    const note = event.target;
+    const noteButton = note.closest('button');
+    const filterItem = note.closest('.catalog-filter-item');
+    const buttonSet = filterItem.querySelector('.filter-btn-set');
+    const filterArrow = filterItem.querySelector('.filter-arrow');
+    const filterWindow = filterItem.querySelector('.filter-window-up');
+    if (noteButton && buttonSet) {
+        filterWindow.classList.toggle('filter-window-active');
+        filterArrow.classList.toggle('active-arrow-up');
+    };
 };
-    
+
+//close filter window if click document
+const filterItem = document.querySelectorAll('.catalog-filter-item');
+document.addEventListener('click', documentOnClick);
+function documentOnClick(event) {
+    filterItem.forEach(function(item) {
+        const withinBoundaries = event.composedPath().includes(item);
+        const filterWindowUp = item.querySelector('.filter-window-up');
+        if ( ! withinBoundaries ) {
+                filterWindowUp.classList.remove('filter-window-active');
+                console.log(filterWindowUp); 
+        };   
+    });  
+};   
+
+// const filterItem = document.querySelector('.catalog-filter-item');
+// const filterWindowUp = document.querySelector('.filter-window-up');
+// document.addEventListener('click', mainOnClick);
+// function mainOnClick(event) {
+//     const withinBoundaries = event.composedPath().includes(filterItem);
+//     if ( ! withinBoundaries ) {  
+//         filterWindowUp.classList.remove('filter-window-active');
+//     };  
+// };   
+
+
+// // in filter window
+// let filterSizeItem = document.querySelectorAll('.filter-size-item');
+// for (let i=0; i<filterSizeItem.length; i++) {
+//     filterSizeItem[i].addEventListener('click', () => {
+//         filterSizeItem[i].classList.toggle('size-red');
+//     });
+// }
+
+// in filter window
+const filterBtnList = document.querySelector('.filter-btn-list');
+filterList.addEventListener('click', filterBtnListOnClick);
+function filterBtnListOnClick(event) {
+    const note = event.target;
+    const filterBtn = note.closest('.filter-btn');
+    if (filterBtn) {
+        note.classList.toggle('size-red');
+    };
+};
+//filterWindow.addEventListener('click', );
+
+// document.addEventListener( 'click', (e) => {
+// 	const withinBoundaries = e.composedPath().includes(filterWindow);
+// 	if ( ! withinBoundaries ) {
+//         console.log('click window');
+// 		filterWindow.classList.toggle('filter-window-active');
+// 	}
+// })
 
 
 // open/close sort mobile-window
